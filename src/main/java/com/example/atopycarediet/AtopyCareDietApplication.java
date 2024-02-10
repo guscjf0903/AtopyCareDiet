46649ff84d7f72fb55250c5cca3c62def42bdda7
hyunchuljung <guscjf0903@naver.com>
2024-02-10 22:52:07 +0900

    Start

---
+++ src/main/java/com/example/atopycarediet/AtopyCareDietApplication.java
@@ -0,0 +1,13 @@
+package com.example.atopycarediet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtopyCareDietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtopyCareDietApplication.class, args);
+    }
+
+}
